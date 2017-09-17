27bc924c7e01f25f4a59317884c15e44ba6e5e9e
Eric W. Greene <eric@ericwgreene.com>
2017-09-17 08:36:12 -0400

    initial commit

---
+++ client/js/components/hello-world.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+
+export class HelloWorld extends React.Component {
+
+  render() {
+    return <Jumbotron fluid>
+      <Container fluid>
+        <h1 className="display-3">Hello World!</h1>
+        <p className="lead">Welcome to React + Redux with TypeScript!</p>
+      </Container>
+    </Jumbotron>;
+  }
+}
