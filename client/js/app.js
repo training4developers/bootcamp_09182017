9af380254bfa5923d2df19cd85ef21522c34f62f
Eric W. Greene <eric@ericwgreene.com>
2017-09-18 14:04:49 -0700

    added styling

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,30 @@
+import '../scss/styles.scss';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { HelloWorld } from './components/hello-world';
+class PersonProfile extends React.Component {
+  render() {
+    // return React.createElement('h1', null, 'Person Profile');
+    return <div>
+      <h1 className="page-title">Person Profile</h1>
+      <ul>
+        <li className="critical">Teja was a billionaire by age 5.</li>
+        <li>Lucy climbed Mt. Everest by age 10.</li>
+        <li>Nika swam across the ocean at age 6.</li>
+      </ul>
+      <h1 className="page-title">Person Profile</h1>
+      <ul>
+        <li className="critical">Teja was a billionaire by age 5.</li>
+        <li>Lucy climbed Mt. Everest by age 10.</li>
+        <li>Nika swam across the ocean at age 6.</li>
+      </ul>
+      <p>In 2015, Tristan sailed the ocean blue.</p>
+    </div>;
+  }
+}
 
-ReactDOM.render(<HelloWorld />, document.querySelector('main'));
+ReactDOM.render(
+  <PersonProfile />,
+  document.querySelector('main')
+);
