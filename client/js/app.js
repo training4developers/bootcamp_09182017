073f6460e56c2a3cefe557a686fe609ddc83b0b4
Eric W. Greene <eric@ericwgreene.com>
2017-09-26 11:30:15 -0700

    added code demos

--- client/js/app.js
+++ client/js/app.js
@@ -1,163 +1,26 @@
 
-Object.assign({}, state);
 
-{ ...state }
+fetch('http://localhost:3010/widgets')
+  .then(res => res.json())
+  .then(widgets => console.log(widgets));
 
-// { name: <some value>, rank: <some value> }
-onSaveFood(food) {
-  this.props.saveFood(food);
-}
+const widget = {
+  name: 'New Widget',
+};
 
-this.componentState.foodName = 'Pizza';
-this.componentState.foodRank = 1;
+fetch('http://localhost:3010/widgets', {
+  method: 'POST',
+  header: { 'Content-Type': 'application/json'},
+  body: JSON.stringify(widget),
+}).then(res => res.json()).then(results => console.log(results));
 
-onSaveFood({
-  name: this.componentState.foodName,
-  rank: this.componentState.foodRank
-});
+const widgetId = "test%56test";
 
-const result = someValue || 2;
-
-const result = state && <div></div>
-
-class MyComp extends React.Component {
+fetch('http://localhost:3010/widgets/' + encodeURIComponent(widgetId), {
+  method: 'DELETE'
+}).then(res => res.json()).then(widget => console.log(widget));
 
 
 
-  render() {
-
-    return React.createElement('div', null,(true) ? <EditRow /> : <ViewRow />)
-
-    // return <div>
-    //   (this.props.editRowId === car.id) ? <EditRow /> : <ViewRow />
-    // </div>;
-
-
-
-    return <div>
-      (do { if (true) <EditRow /> else <ViewRow /> })
-    </div>
-
-    if (true) {
-      return <div><EditRow /></div>;
-    } else {
-      return <div><ViewRow /></div>;
-    }
-
-  }
-
-}
-
-
-// const state = {
-//   foods: [
-//     { name: 'pizza', rank: 1 },
-//     { name: 'chicken nuggets', rank: 2 }
-//   ],
-//   drinks: [
-//     { name: 'water', rank: 1 },
-//     { name: 'sode', rank: 2 },
-//   ],
-// };
-
-// class FoodList extends React.Component {
-
-//   render() {
-//     this.props.foodList
-//     this.props.drinksList
-//   }
-
-// }
-
-// const mapStateToProps = ({ foods, drinks }) => ({
-//   foodList: foods.map(({ name }) => name.toUpperCase()), drinksList: drinks
-// });
-
-// const props = mapStateToProps(state);
-
-
-
-
-// const name = 'Bob';
-
-// const propName = 'age';
-
-
-// const p = {
-//   _name: name,
-//   displayName() {
-//     console.log(this._name);
-//   },
-//   [ propName ]: 67,
-//   get name() {
-//     return this._name;
-//   },
-//   set name(value) {
-//     this._name = value;
-//   }
-// };
-
-// console.log(p);
-// p.displayName();
-
-
-
-// const p = {
-//   address: {
-//     street: {
-//       number: 1,
-//       name: 'Oak Lane'
-//     }
-//     city: 
-//   }
-// }
-
-// const { street, city } = p.address.street;
-
-// street.
-
-// function doIt2({ url }, ) {
-//   url
-// }
-
-// const options = {
-//   url: 'http://www.intuit.com',
-// };
-
-// doIt2(options)
-
-
-
-// function doIt(a,b,c,...params) {
-//   console.log(a,b,c, params);
-// }
-
-// doIt(...[1,2,3,4,5], 6);
-
-// const nums = [1,2,3].concat(4);
-// console.log(nums);
-
-// const [ , ...items ]  = [ 1,2,3,4,5 ];
-
-// const items = [1,2,3,4,5].slice(1);
-
-// console.log(items);
-
-
-
-// const p = { 
-//   firstName: 'Bob',
-//   lastName: 'Smith',
-//   address: {
-//     city: 'Parker',
-//     state: 'CO',
-//   }
-// };
-
-// const firstName = 'Sarah';
-// //        const fn = firstName;   const { city } = address
-// const { firstName: fn, lastName, address: { city : c } } = p;
-
-// console.log(firstName, fn, lastName, c);
 
 
