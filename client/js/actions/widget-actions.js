ffe315f7bc7af9c84bb71c559a800bf4351d90da
Eric W. Greene <eric@ericwgreene.com>
2017-09-27 07:03:52 -0700

    cleaned up code

--- client/js/actions/widget-actions.js
+++ client/js/actions/widget-actions.js
@@ -1,11 +1,5 @@
 import { actionTypes } from '../action-types';
 
-export const replaceWidgetActionCreator =
-  widget => ({ type: actionTypes.REPLACE, widget });
-
-export const deleteWidgetActionCreator =
-  widgetId => ({ type: actionTypes.DELETE, widgetId });
-
 export const editWidgetActionCreator =
   widgetId => ({ type: actionTypes.EDIT, widgetId });
 
