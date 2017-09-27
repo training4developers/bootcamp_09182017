ecfba568aeae3b432b94ce898d6c1b635c95894d
Eric W. Greene <eric@ericwgreene.com>
2017-09-27 11:15:11 -0700

    added code demos

---
+++ client/js/app-lifecycle.js
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class Third extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor: third');
+  }
+
+  componentWillMount() {
+    console.log('component will mount: third');
+  }
+
+  componentDidMount() {
+    console.log('component did mount: third');
+  }
+
+  componentWillReceiveProps() {
+    console.log('component will receive props: third');
+  }
+
+  componentWillUpdate() {
+    console.log('component will update: third');
+  }
+
+  componentDidUpdate() {
+    console.log('component did update: third');
+  }
+
+  render() {
+    console.log('render: third');
+    return <div>
+      Third {this.props.num}
+    </div>;
+  }
+}
+
+class Second extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    console.log('constructor: second');
+  }
+
+  componentWillMount() {
+    console.log('component will mount: second');
+  }
+
+  componentDidMount() {
+    console.log('component did mount: second');
+  }
+
+  componentWillReceiveProps() {
+    console.log('component will receive props: second');
+  }
+
+  componentWillUpdate() {
+    console.log('component will update: second');
+  }
+
+  componentDidUpdate() {
+    console.log('component did update: second');
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+
+  //   console.log('should component update: second');
+
+  //   const keys = Object.keys(this.props);
+  //   let shouldUpdate = false;
+
+  //   keys.forEach(key => {
+  //     if (this.props[key] !== nextProps[key]) {
+  //       shouldUpdate = true;
+  //     }
+  //   });
+
+  //   return shouldUpdate;
+  // }
+  
+  render() {
+    console.log('render: second');
+    return <div>
+      Second {this.props.num}
+      <Third {...this.props} />
+    </div>;
+  }
+}
+
+class First extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor: first');
+  }
+
+  componentWillMount() {
+    console.log('component will mount: first');
+  }
+
+  componentDidMount() {
+    console.log('component did mount: first');
+  }
+
+  componentWillReceiveProps() {
+    console.log('component will receive props: first');
+  }
+
+  componentWillUpdate() {
+    console.log('component will update: first');
+  }
+
+  componentDidUpdate() {
+    console.log('component did update: first');
+  }
+  
+  render() {
+    console.log('render: first');
+    return <div>
+      First {this.props.num}
+      <Second num={42} />
+    </div>;
+  }
+}
+
+setTimeout(() => {
+
+  ReactDOM.render(<First num={27} />, document.querySelector('main'));
+
+}, 5000);
+
+
+ReactDOM.render(<First num={42} />, document.querySelector('main'));
+
--- client/js/app.js
+++ client/js/app.js
@@ -1,116 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { connect, Provider } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-class Third extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('constructor: third');
-  }
+import { appStore } from './app-store';
+import * as widgetActionCreators from './actions/widget-actions';
+import { refreshWidgets } from './actions/refresh-widgets';
+import { insertWidget } from './actions/insert-widget';
+import { deleteWidget } from './actions/delete-widget';
+import { replaceWidget } from './actions/replace-widget';
+import { WidgetTool } from './components/widget-tool';
 
-  componentWillMount() {
-    console.log('component will mount: third');
-  }
+const WidgetToolContainer = connect(
+  ({ widgets, editWidgetId }) => ({ widgets, editWidgetId }),
+  dispatch => bindActionCreators({
+    insertWidget,
+    replaceWidget,
+    deleteWidget,
+    editWidget: widgetActionCreators.editWidgetActionCreator,
+    cancelWidget: widgetActionCreators.cancelWidgetActionCreator,
+    refreshWidgets,
+  }, dispatch))(WidgetTool);
 
-  componentDidMount() {
-    console.log('component did mount: third');
-  }
-
-  componentWillReceiveProps() {
-    console.log('component will receive props: third');
-  }
-
-  componentWillUpdate() {
-    console.log('component will update: third');
-  }
-
-  componentDidUpdate() {
-    console.log('component did update: third');
-  }
-
-  render() {
-    console.log('render: third');
-    return <div>
-      Third {this.props.num}
-    </div>;
-  }
-}
-
-class Second extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('constructor: second');
-  }
-
-  componentWillMount() {
-    console.log('component will mount: second');
-  }
-
-  componentDidMount() {
-    console.log('component did mount: second');
-  }
-
-  componentWillReceiveProps() {
-    console.log('component will receive props: second');
-  }
-
-  componentWillUpdate() {
-    console.log('component will update: second');
-  }
-
-  componentDidUpdate() {
-    console.log('component did update: second');
-  }
-  
-  render() {
-    console.log('render: second');
-    return <div>
-      Second {this.props.num}
-      <Third {...this.props} />
-    </div>;
-  }
-}
-
-class First extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('constructor: first');
-  }
-
-  componentWillMount() {
-    console.log('component will mount: first');
-  }
-
-  componentDidMount() {
-    console.log('component did mount: first');
-  }
-
-  componentWillReceiveProps() {
-    console.log('component will receive props: first');
-  }
-
-  componentWillUpdate() {
-    console.log('component will update: first');
-  }
-
-  componentDidUpdate() {
-    console.log('component did update: first');
-  }
-  
-  render() {
-    console.log('render: first');
-    return <div>
-      First {this.props.num}
-      <Second {...this.props} />
-    </div>;
-  }
-}
-
-setTimeout(() => {
-
-  ReactDOM.render(<First num={27} />, document.querySelector('main'));
-
-}, 5000);
-
-
-ReactDOM.render(<First num={42} />, document.querySelector('main'));
-
+ReactDOM.render(<Provider store={appStore}>
+  <WidgetToolContainer />
+</Provider>, document.querySelector('main'));
