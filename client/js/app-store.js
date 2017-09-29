811f98f0ff4a3d0d6e193bbc8cd2700440f5f3a2
Eric W. Greene <eric@ericwgreene.com>
2017-09-28 17:01:25 -0700

    added redux tools config

--- client/js/app-store.js
+++ client/js/app-store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 
@@ -11,9 +11,20 @@ import { replaceWidgetSaga } from './actions/replace-widget';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk, sagaMiddleware),
+);
+
 export const appStore = createStore(
   combineReducers({ widgets, editWidgetId }),
-  applyMiddleware(thunk, sagaMiddleware),
+  enhancer,
 );
 
 sagaMiddleware.run(refreshWidgetsSaga);
